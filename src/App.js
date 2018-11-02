cef62e0da03d53e987adc7ddafc30e20cbf87a24
Tehreem Javid <tehreemjavid@Tehreems-MacBook-Pro-2.local>
2018-11-02 11:47:33 -0700

    added comp for fraud

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import FraudList from './components/FraudList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <FraudList />
       </div>
     );
   }
---
+++ src/components/FraudList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import FraudItem from './FraudItem'
+
+class FraudList extends Component {
+  render() {
+    return (
+      <div>
+        <FraudItem />
+      </div>
+    )
+  }
+}
+
+
+export default FraudList
